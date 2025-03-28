e32e0512edf6bbe958f0501956ae08eef6380e66
121Artem122 <99825505+121Artem122@users.noreply.github.com>
2025-03-28 20:03:09 +0400

    no

--- api-server/src/main/java/com/example/apigateway/controller/UserController.java
+++ api-server/src/main/java/com/example/apigateway/controller/UserController.java
@@ -5,7 +5,10 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -44,7 +47,6 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable Long id) {
         String url =  coreServiceUrl +"/"+ id;  // Формируем URL для Core сервиса
         restTemplate.exchange(url, HttpMethod.DELETE, null, Void.class);
--- api-server/src/main/java/com/example/apigateway/dto/User.java
+++ api-server/src/main/java/com/example/apigateway/dto/User.java
@@ -1,10 +1,8 @@
 package com.example.apigateway.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class User {
     private Long id;
 
